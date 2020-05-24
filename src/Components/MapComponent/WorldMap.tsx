fb0a80d687dadf8ff6582c3d713975bc4adf067b
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-24 19:11:03 +0300

    utilize mapbox raster tilelayer & add Makers on affected countries

--- src/Components/MapComponent/WorldMap.tsx
+++ src/Components/MapComponent/WorldMap.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import {Map, TileLayer} from 'react-leaflet';
+import React, {useEffect, useState} from 'react';
+import {Map, TileLayer, Marker} from 'react-leaflet';
 import {LatLngTuple} from 'leaflet';
 import './WorldMap.css';
+import {getCountryStats, IStatData} from '../../Api';
 
 function WorldMap() {
     const defaultCenter: LatLngTuple = [9.1021, 18.2812];
-    const defaultZoom: number = 3.0;
+    const defaultZoom: number = 3.2;
+
+    const [countryStats, setCountryStats] = useState<IStatData[]>();
+
+    useEffect(() => {
+        (async() => {
+            const stats = await getCountryStats();
+            if(stats) 
+                setCountryStats(stats);
+            else 
+                console.error(`Failed to fetch country Stats`);
+        })();
+    }, []);
 
     return (
         <Map id = "worldmap" center = {defaultCenter} zoom = {defaultZoom}>
             <TileLayer 
-            attribution= "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-            url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution= {
+                `Map data &copy; 
+                <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, 
+                <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, 
+                Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>`
+            }
+            url = {
+                `https://api.mapbox.com/styles/v1/immanuel-wandeto/ckakxi28w3ajz1io1yelozgq9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`
+            }
+            maxZoom = {4.2}
+            minZoom = {3.0}
             />
+            {countryStats && countryStats.map(country => {
+                const latLng: LatLngTuple = [country.latLng.latitude, country.latLng.longitude]
+                return (
+                    <Marker position = {latLng} key = {countryStats.indexOf(country)}>
+
+                    </Marker>)
+            })}
         </Map>
     );
 }
