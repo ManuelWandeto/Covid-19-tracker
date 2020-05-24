4b3a11236423826c31f88c62ca5b26176ca0c9b5
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-24 21:26:56 +0300

    update api to return global stats rather than only countries

--- src/Api.ts
+++ src/Api.ts
@@ -5,23 +5,30 @@ export interface IStatData {
         latitude: number,
         longitude: number,
     },
+    confirmed: number,
     active: number,
     recovered: number,
     critical: number,
     deaths: number
 }
 
-export async function getCountryStats() {
+export interface WorldwideStats {
+    worldwide: IStatData,
+    countries: IStatData[]
+}
+
+export async function getWorldWideStats() {
     try {
         const response = await fetch('https://us-central1-covid-tracker-api-c2a95.cloudfunctions.net/api/globalStats');
         const stats = await response.json();
-
-        const countries = stats?.countries;
-        if(countries) {
-            return countries as IStatData[];
-        } else {
-            throw new Error('fetch operation yielded non truthy data');
+        const worldwideStats: WorldwideStats = {
+            worldwide: stats.worldwide,
+            countries: stats.countries
         }
+        if(worldwideStats) 
+            return worldwideStats;
+        else 
+            throw new Error(`fetch operation yielded non truthy data`);
     } catch (error) {
         console.error(`an error occured: ${error}`)
     }
