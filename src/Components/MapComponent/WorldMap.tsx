6793da79fc5275f2cbf9cac55cb5b86dc3a58608
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-24 21:27:37 +0300

    update circlemarker with custom styling

--- src/Components/MapComponent/WorldMap.tsx
+++ src/Components/MapComponent/WorldMap.tsx
@@ -2,19 +2,19 @@ import React, {useEffect, useState} from 'react';
 import {Map, TileLayer, CircleMarker, Popup} from 'react-leaflet';
 import {LatLngTuple} from 'leaflet';
 import './WorldMap.css';
-import {getCountryStats, IStatData} from '../../Api';
+import {getWorldWideStats, WorldwideStats} from '../../Api';
 
 function WorldMap() {
     const defaultCenter: LatLngTuple = [9.1021, 18.2812];
-    const defaultZoom: number = 3.2;
-
-    const [countryStats, setCountryStats] = useState<IStatData[]>();
+    const defaultZoom: number = 4.0;
+    
+    const [globalStats, setGlobalStats] = useState<WorldwideStats>();
 
     useEffect(() => {
         (async() => {
-            const stats = await getCountryStats();
+            const stats = await getWorldWideStats();
             if(stats) 
-                setCountryStats(stats);
+                setGlobalStats(stats);
             else 
                 console.error(`Failed to fetch country Stats`);
         })();
@@ -32,16 +32,26 @@ function WorldMap() {
             url = {
                 `https://api.mapbox.com/styles/v1/immanuel-wandeto/ckakxi28w3ajz1io1yelozgq9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`
             }
-            maxZoom = {4.2}
+            maxZoom = {6.5}
             minZoom = {3.0}
             />
-            {countryStats && countryStats.map(country => {
+            {globalStats && globalStats.countries.map(country => {
                 const latLng: LatLngTuple = [country.latLng.latitude, country.latLng.longitude]
+                let baseRadius = 15;
+                let averageGlobalCases = globalStats.worldwide.confirmed / globalStats.countries.length;
                 return (
-                    <CircleMarker center = {latLng} radius ={15} key = {countryStats.indexOf(country)}>
+                    <CircleMarker 
+                    key = {globalStats.countries.indexOf(country)}
+                    center = {latLng} 
+                    radius ={baseRadius + Math.log(country.confirmed / averageGlobalCases)} 
+                    fillOpacity = {.1}
+                    weight = {2}
+                    fillColor = "#EE2934"
+                    color = "#EE2934"
+                    opacity = {.8}
+                    >
                         <Popup>
                             <h1>{country.countryName}</h1>
-
                         </Popup>
                     </CircleMarker>
                 )
