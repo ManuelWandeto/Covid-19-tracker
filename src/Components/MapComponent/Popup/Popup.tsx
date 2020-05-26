902d9d2538c26ee096952d4fb17d0050f7ad853d
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-26 16:14:57 +0300

    update presentational logic of a popup

--- src/Components/MapComponent/Popup/Popup.tsx
+++ src/Components/MapComponent/Popup/Popup.tsx
@@ -16,6 +16,8 @@ interface IPopupProps {
 
 
 const CountryPopup :React.FC<IPopupProps> = (props) => {
+    const {name, code, active, confirmed, critical, deaths, recovered} = props;
+
     return (
         <Popup>
             <div className = "popup-content">
@@ -23,13 +25,26 @@ const CountryPopup :React.FC<IPopupProps> = (props) => {
                     <ReactCountryFlag 
                     svg = {true}
                     className = "countryFlag" 
-                    countryCode = { props.code } 
-                    aria-label = {props.name}/>
-                    {props.name}
+                    countryCode = { code } 
+                    aria-label = {name}/>
+                    {name}
                 </h2>
+                <li>Confirmed:<span>{formatter(confirmed)}</span></li>
+                <li>Active:<span>{formatter(active)}</span></li>
+                <li>Recovered:<span>{formatter(recovered)}</span></li>
+                <li>critical:<span>{formatter(critical)}</span></li>
+                <li>Deaths:<span>{formatter(deaths)}</span></li>
             </div>
         </Popup>
     )
+}
+
+function formatter(value: number): string {
+    if(value < 0) {
+        return 'Unknown';
+    } else {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 }
  
 export default CountryPopup;
