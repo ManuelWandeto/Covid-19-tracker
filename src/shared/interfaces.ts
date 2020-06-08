b42b257f04962062cf6b288c41ad51c89902950a
ManuelWandeto <immanuelmojo@hotmail.com>
2020-06-08 20:38:40 +0300

    add shared interfaces file

---
+++ src/shared/interfaces.ts
@@ -0,0 +1,31 @@
+export interface StateData {
+    [index: string]: any,
+    name: string,
+    countryCode: string,
+    stateCode?: string,
+    states?: StateData[],
+    latLng: {
+        latitude: number,
+        longitude: number,
+    }
+    population: number,
+    tests: number,
+    confirmed: number,
+    active: number,
+    recovered: number,
+    critical: number,
+    deaths: number,
+}
+
+export interface GlobalStats {
+    updatedAt: Date,
+    worldwide: {
+        tests?: number,
+        confirmed: number,
+        active: number,
+        recovered: number,
+        critical: number,
+        deaths: number
+    },
+    countries: StateData[]
+}
